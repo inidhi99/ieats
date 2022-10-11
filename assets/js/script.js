4a857915af38d79b001a7ead43401a8402ddbcfc
JAG <jagrasso42@gmail.com>
2022-10-11 12:21:09 -0400

    resolved merge conflicts

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,7 +68,7 @@ function initGoogle() {
     });
     
   // Listen for autocomplete selection  
-  varautocomplete.addListener('place_changed', () => {
+  autocomplete.addListener('place_changed', () => {
     var place = autocomplete.getPlace();
     placeMarker = new google.maps.Marker({
       placeId: place.place_id,
