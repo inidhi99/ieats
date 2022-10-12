19106cf3e314e7d0e7125f03434298a47aba913b
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-10-12 09:47:00 -0400

    styling layout edit and JS note added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var googleKey = 'AIzaSyDh2jcs3sWSy_5L5y-hdC0bryjDAjOEZTg';
 var weatherKey = '66b15a5b3951d15de56c5d2c4e2ddcba';
 var inputEl = document.getElementById('autocomplete')
 var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=40.7127281&lon=-74.0060152&appid=66b15a5b3951d15de56c5d2c4e2ddcba&units=imperial"
-var weatherWidget= document.querySelector(".weather")
+var weatherWidget = document.querySelector(".weather")
 var markers = [];
 var placeMarker;
 
@@ -12,7 +12,7 @@ var placeMarker;
 // DEPENDENCIES
 var autocomplete;
 
- 
+
 
 // FUNCTIONS
 function getLatLon(city) {
@@ -21,25 +21,25 @@ function getLatLon(city) {
     return response.json()
   }).then(data => {
     console.log(data);
-  }) 
+  })
 }
 
 // function to get weather 
-  async function getWeather(weatherUrl){
-    const response = await fetch(weatherUrl);
-     var data = await response.json();
-      console.log(data);
-      var temp= data.main.temp;
-      var feelsLike= data.main.feels_like;
-      var weatherDesc= data.weather[0].main;
-       console.log(feelsLike);
-       console.log(temp);
-       console.log(weatherDesc)
-       document.getElementById('weather').innerHTML= "Temperature: " + temp + " Feels Like: "+ feelsLike + ", Conditions: " + weatherDesc ;
-      }
+async function getWeather(weatherUrl) {
+  const response = await fetch(weatherUrl);
+  var data = await response.json();
+  console.log(data);
+  var temp = data.main.temp;
+  var feelsLike = data.main.feels_like;
+  var weatherDesc = data.weather[0].main;
+  console.log(feelsLike);
+  console.log(temp);
+  console.log(weatherDesc)
+  document.getElementById('weather').innerHTML = "Temperature: " + temp + " Feels Like: " + feelsLike + ", Conditions: " + weatherDesc;
+}
 
 // function initAutocomplete() { 
-  // }
+// }
 
 function initGoogle() {
   var newYorkLatLon = { lat: 40.7127281, lng: -74.0060152 }
@@ -65,31 +65,32 @@ function initGoogle() {
   const input = document.getElementById('map-input');
 
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
-  
+
   // Add marker
   var newYorkMarker = new google.maps.Marker({
     position: newYorkLatLon,
     map: map,
     icon: './assets/images/icons/blue-dot.png'
   });
-  
+
   // Add event listener to marker
-  newYorkMarker.addListener('click', function(){
+  newYorkMarker.addListener('click', function () {
     infoWindow.open(map, newYorkMarker);
   });
-  
+
   autocomplete = new google.maps.places.Autocomplete(
     input,
     {
       bounds: defaultBounds,
-      componentRestrictions: {'country': ['us']},
+      componentRestrictions: { 'country': ['us'] },
       fields: ['place_id', 'geometry', 'name', 'adr_address'],
       types: ['restaurant', 'cafe'] // types: ['restaurant', 'cafe'], types: ['establishment']
     });
-    
+
   // Listen for autocomplete selection  
   autocomplete.addListener('place_changed', () => {
     var place = autocomplete.getPlace();
+    console.log(place)
     placeMarker = new google.maps.Marker({
       placeId: place.place_id,
       address: place.adr_address,
@@ -97,19 +98,21 @@ function initGoogle() {
       title: place.name,
       map: map
     });
-
+    console.log(placeMarker)
     var placeInfoWindow = new google.maps.InfoWindow({
-      content: 
-      `
+      content:
+        `
         <h2>${placeMarker.title}</h2>
         <p>${placeMarker.address}</p>
+        
+        
       `
     })
-    placeMarker.addListener('click', function(){
-      placeInfoWindow.open(map, placeMarker); 
+    placeMarker.addListener('click', function () {
+      placeInfoWindow.open(map, placeMarker);
     });
   });
 }
-
+//Tutor recommendation Add element tags in placeMarker section..in ref to image, open hours, price range//
 getLatLon(targetCity);
 getWeather(weatherUrl);
