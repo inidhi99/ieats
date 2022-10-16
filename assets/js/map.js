07a62b2feff284a593f8cf1e757d650a89dee4ce
JAG <jagrasso42@gmail.com>
2022-10-16 16:14:53 -0400

    added newBounds variable to switch statement

--- assets/js/map.js
+++ assets/js/map.js
@@ -38,23 +38,33 @@ function setMapDisplay() {
 function initGoogle() {
   // variable for initial center of map
   var newYorkLatLon = { lat: 40.7127281, lng: -74.0060152 };
-  // options for google map
-  var options = {
-    zoom: cityZoom,
-    center: newYorkLatLon,
-    disableDefaultUI: true,
-    mapTypeControl: false, // remove Map/Satellite buttons
-    zoomControl: false,
-    fullscreenControl: false,
-    gestureHandling: 'none'
-  };
-  // New map
-  const map = new google.maps.Map(document.getElementById("map"), options);
-
   // lat and lon for specific search areas
   var manhattanLatLon = { lat: 40.7831, lng: -73.9712 };
   var brooklynLatLon = { lat: 40.6782, lng: -73.9442 };
   var queensLatLon = { lat: 40.7282, lng: -73.7949 };
+  var cityBounds = {
+    north: newYorkLatLon.lat + 0.15,
+    south: newYorkLatLon.lat - 0.05,
+    east: newYorkLatLon.lng + 0.15,
+    west: newYorkLatLon.lng - 0.05,
+  };
+  // options for google map
+  var options = {
+    zoom: cityZoom,
+    center: newYorkLatLon,
+    LatLngBounds: cityBounds,
+    disableDefaultUI: true,
+    mapTypeControl: false // remove Map/Satellite buttons
+    // zoomControl: false,
+    // fullscreenControl: false,
+    // gestureHandling: 'none'
+  };
+  // New map
+  const map = new google.maps.Map(document.getElementById("map"), options);
+
+  console.log(map.getCenter());
+
+
 
   // get the search bar element
   const inputEl = document.getElementById('map-input');
@@ -77,12 +87,15 @@ function initGoogle() {
     switch (e.target.id) {
       case 'changetype-all':
         // move the center of the map to New York City's lattitude and longitude
+        console.log(map.getCenter().lat());
         map.setCenter({ lat: newYorkLatLon.lat, lng: newYorkLatLon.lng });
+        console.log(map.getCenter());
         // match the marker location with the new map center
         centerMarker.setPosition(new google.maps.LatLng(newYorkLatLon.lat, newYorkLatLon.lng));
         // zoom out for view of whole city
         map.setZoom(cityZoom);
-        autocomplete.bindTo('bounds', map);
+        console.log(map.getBounds());
+        // map.setBounds();
         getWeather(newYorkLatLon.lat, newYorkLatLon.lng);
         break;
         case 'changetype-manhattan':
@@ -91,38 +104,56 @@ function initGoogle() {
           centerMarker.setPosition(new google.maps.LatLng(manhattanLatLon.lat, manhattanLatLon.lng));
           // zoom in for view of individual boroughs
           map.setZoom(boroughZoom);
-          autocomplete.bindTo('bounds', map);
+          console.log(map.getBounds());
+          
+          // map.setBounds();
           getWeather(manhattanLatLon.lat, manhattanLatLon.lng);
-        break;
-      case 'changetype-brooklyn':
-        // move the center of the map to Brooklyn's lattitude and longitude
-        map.setCenter({ lat: brooklynLatLon.lat, lng: brooklynLatLon.lng, boroughZoom });
-        centerMarker.setPosition(new google.maps.LatLng(brooklynLatLon.lat, brooklynLatLon.lng));
-        map.setZoom(boroughZoom);
-        autocomplete.bindTo('bounds', map);
-        getWeather(brooklynLatLon.lat, brooklynLatLon.lng);
-        break;
-      case 'changetype-queens':
-        // move the center of the map to Queens' lattitude and longitude
-        map.setCenter({ lat: queensLatLon.lat, lng: queensLatLon.lng, boroughZoom });
-        centerMarker.setPosition(new google.maps.LatLng(queensLatLon.lat, queensLatLon.lng));
-        map.setZoom(boroughZoom);
-        autocomplete.bindTo('bounds', map);
-        getWeather(queensLatLon.lat, queensLatLon.lng);
-        break;
-    }
-  })
+          break;
+          case 'changetype-brooklyn':
+            // move the center of the map to Brooklyn's lattitude and longitude
+            map.setCenter({ lat: brooklynLatLon.lat, lng: brooklynLatLon.lng, boroughZoom });
+            centerMarker.setPosition(new google.maps.LatLng(brooklynLatLon.lat, brooklynLatLon.lng));
+            map.setZoom(boroughZoom);
+            console.log(map.getBounds());
+            // map.setBounds();
+            getWeather(brooklynLatLon.lat, brooklynLatLon.lng);
+            break;
+            case 'changetype-queens':
+              // move the center of the map to Queens' lattitude and longitude
+              map.setCenter({ lat: queensLatLon.lat, lng: queensLatLon.lng, boroughZoom });
+              centerMarker.setPosition(new google.maps.LatLng(queensLatLon.lat, queensLatLon.lng));
+              map.setZoom(boroughZoom);
+              console.log(map.getBounds());
+              // map.setBounds();
+              getWeather(queensLatLon.lat, queensLatLon.lng);
+              break;
+            }
+            var newBounds = {
+              north: map.getCenter().lat() + 0.07,
+              south: map.getCenter().lat() - 0.07,
+              east: map.getCenter().lng() + 0.7,
+              west: map.getCenter().lng() - 0.7,
+            }
+            console.log(map.getBounds());
+            // map.setBounds(newBounds);
+            autocomplete.bindTo('bounds', map);
+          });
+          
+          
+          
+  autocompleteOptions = {
+    componentRestrictions: { 'country': ['us'] },
+    fields: ['place_id', 'geometry', 'name', 'adr_address'],
+    types: ['restaurant', 'cafe'] // specific types: ['restaurant', 'cafe'], general type: ['establishment']
+  }
 
   // add autocomplete to search bar element
   autocomplete = new google.maps.places.Autocomplete(
-    inputEl,
-    {
-      componentRestrictions: { 'country': ['us'] },
-      fields: ['place_id', 'geometry', 'name', 'adr_address'],
-      types: ['restaurant', 'cafe'] // specific types: ['restaurant', 'cafe'], general type: ['establishment']
-    });
+    inputEl, autocompleteOptions);
 
-  // restric the bounds of the search to the area visible on the map
+  // Bind the map's bounds (viewport) property to the autocomplete object,
+  // so that the autocomplete requests use the current map bounds for the
+  // bounds option in the request.
   autocomplete.bindTo('bounds', map);
 
   // Listen for autocomplete selection  
@@ -222,9 +253,9 @@ function setServiceOptionsStrings(serciveOptionsObj) {
 
   if (serciveOptionsObj.delivery === true) {
     // global string to be rendered in restaurantContainerElement innerHTML
-    deliveryString = 'Dine in available!';
+    deliveryString = 'Delivery available!';
   } else {
-    deliveryString = 'Dine in unavailable';
+    deliveryString = 'Delivery unavailable';
   }
 }
 
